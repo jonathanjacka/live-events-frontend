9c96283ef7c1a68e0328de20c59c4493f2cf3415
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-02 04:59:44 -0500

    Added import path objects, Showcase component for homepage

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export const Showcase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To The Jol!</h1>
+      <h2>Find the Kiffest Live Events!</h2>
+    </div>
+  );
+};
