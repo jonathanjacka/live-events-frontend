6522582d79e492246d41557707f95569b5d15e78
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-02 06:13:36 -0500

    Added /pages/index.js get props functionality and associated files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import { Layout } from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+//runs first visit only, validate will update only if change detected...
+//Why not use getServerSideProps() instead?
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+}
+
+export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
 }
