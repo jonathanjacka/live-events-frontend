5e95468d9adb7a756cf3968748689f3ddde6c8fb
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-03 06:44:25 -0500

    Updated event and event page, and eventItem to use strapi api

--- pages/events/index.js
+++ pages/events/index.js
@@ -5,7 +5,7 @@ import { API_URL } from '@/config/index';
 //runs first visit only, validate will update only if change detected...
 //Why not use getServerSideProps() instead?
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
   const events = await res.json();
 
   return {
