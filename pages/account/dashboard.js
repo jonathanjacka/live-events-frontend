997df46086d3f2f252f2a714dd4fe885a03a21c9
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-09 06:08:44 -0500

    Added DasbBoard component and dashboard display of user created events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,47 @@
 import { Layout } from '@/components/Layout';
+import { DashboardEvent } from '@/components/DashboardEvent';
+import { API_URL } from '@/config/index';
+import { parseCookies } from '@/utils/parseCookies';
+import styles from '@/styles/Dashboard.module.css';
 
-export default function DashboardPage() {
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
+}
+
+export default function DashboardPage({ events }) {
+  const deleteEvent = (id) => {
+    console.log(`DELETE ME: ${id}`);
+  };
+
   return (
     <Layout title='User Dashboard'>
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events ? (
+          events.map((evt) => (
+            <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+          ))
+        ) : (
+          <h3>You haven't added any events yet</h3>
+        )}
+      </div>
     </Layout>
   );
 }
